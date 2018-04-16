6fe35f802a8da12a5e67b6274e393fdf214f456e
Jim <jp.alejandro01@gmail.com>
2018-04-16 15:58:17 -0600

    added fromrspree

---
+++ controller/php.php
@@ -0,0 +1,10 @@
+<?php
+
+$con =   new mysqli('localhost','root','','oxxo');
+mysqli_query ($con, )
+
+if  (mysqli_connect_errno()){
+    echo "Failed to connect"
+    mysqli_connect_errno();
+}
+?>
